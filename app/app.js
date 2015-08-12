1f7ce264f7f5ca49a13523f5e2dfb8b5ad24f1be
Brant Choate <brantchoate@Brants-MacBook-Pro.local>
2015-08-12 15:38:14 -0700

    added comments to the app.js

--- app/app.js
+++ app/app.js
@@ -1,3 +1,10 @@
+/*
+These imports work at the namespaces because of the Ember Resolver
+Naming Convention here - http://www.ember-cli.com/using-modules/
+Ember is a special case - http://stackoverflow.com/questions/25276647/how-does-ember-cli-know-where-to-import-ember-data-from
+IN CHOWNOW APP these module names are created by a gulp process that wraps each module in an AMD definition
+*/
+
 import Ember from 'ember';
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
@@ -5,13 +12,31 @@ import config from './config/environment';
 
 var App;
 
+/*
+This is to allow dependency injection into ember data models
+http://stackoverflow.com/questions/24068890/injecting-dependencies-into-an-ember-model
+*/
+
 Ember.MODEL_FACTORY_INJECTIONS = true;
+
+/*
+Ember.Application is the container that holds your other classes in your app
+http://emberjs.com/api/classes/Ember.Application.html
+Ember.Applicaiton.extend is the way to create new subclasses (the params passed in are mixins)
+http://emberjs.com/api/classes/Ember.Application.html#method_extend
+
+*/
 
 App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
-  Resolver: Resolver
+  Resolver: Resolver,
 });
+
+/*
+TODO: Not sure what initializers really are
+https://github.com/ember-cli/ember-load-initializers
+*/
 
 loadInitializers(App, config.modulePrefix);
 
